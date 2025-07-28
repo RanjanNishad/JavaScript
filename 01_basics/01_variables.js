6805d044cbcdf87e99acf21c8419a542f3a310d1
Ranjan Kumar <109688694+RanjanNishad@users.noreply.github.com>
2025-07-28 16:28:48 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accId = 1234443
+let accEmail = "ranjan@gmail.com"
+var accPwd = "r123"
+accCity = "Muzaffarpur"
+let accState ;
+
+// single print
+console.log(accId);
+console.table([accId,accEmail,accPwd,accCity,accState])
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,29 @@
+console.log("Ranjan")
+"use strict"; //treat all JS code newer version
+// alert(2+3); // we are using node js , not browser
+
+console.log(3
+    +
+    3
+) //code readability shpuld be high
+console.log("Ranjan")
+
+let name = "Ranjan"
+let age = 21
+let isLoggedIn = false
+
+// data type
+// num => 2 to power 53
+// bigint 
+// String = ""
+// boolean => true/false
+// null => standalone value
+// undefine =>
+// symbol=>unidue
+
+// object
+
+console.log(typeof "Raj");
+console.log(typeof null);
+console.log(typeof undefined);
+
